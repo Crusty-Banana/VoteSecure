ec48a047b71b005b69237ba247066de25f34c297
CrustyBanana <dodoankhac@gmail.com>
2024-02-02 00:20:12 +0700

    fix: only admin can create more admin

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -4,7 +4,7 @@ const userController = require("../controllers/userControllers");
 const router = express.Router();
 const auth = require("../middleware/auth");
 
-router.route("/register").post(userController.register);
+router.route("/register").post(auth, userController.register);
 router.route("/login").post(userController.login);
 router.route("/auth-test").post(auth, (req, res) => {
   return res.status(234).send("authenticated!");
